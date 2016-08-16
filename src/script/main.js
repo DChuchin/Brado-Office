a0b12f039f40b39731012dd95ac8dad753c6a996
DmitryChuchin <dime10cents@gmail.com>
2016-08-16 17:28:13 +0600

    stylize 07_page

--- src/script/main.js
+++ src/script/main.js
@@ -1,17 +1,31 @@
-$('.bottom-carousel__body').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    infinite: true,
-    asNavFor: '.product-detail__main-slider'
-});
-$('.product-detail__main-slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: '.bottom-carousel__body',
-    infinite: true
-});
-$('.single-news__carousel').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    infinite: true
+$(document).ready(function() {
+    $('.bottom-carousel__body').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        asNavFor: '.product-detail__main-slider'
+    });
+    $('.product-detail__main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.bottom-carousel__body',
+        infinite: true
+    });
+    $('.single-news__carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true
+    });
+})
+
+document.addEventListener('DOMContentLoaded',function() {
+    console.log('load');
+    let cookieLink = document.querySelector('.cookie__close-btn');
+
+    if (cookieLink) {
+        cookieLink.addEventListener('click', function(e){
+            e.preventDefault();
+            cookieLink.parentElement.style.display= 'none';
+        });
+    };
 });
